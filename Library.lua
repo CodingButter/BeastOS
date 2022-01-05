96ae59736e314c1c22da1245dc9ec0eae53d7921
Jamie Nichols <Jamie337nichols@gmail.com>
2022-01-05 15:36:49 -0500

    update

--- Library.lua
+++ Library.lua
@@ -4,55 +4,8 @@
 	Email: Jamie337nichols@gmail.com
 ]]
 
-
-
---[[UTILITY FUNCTIONS]]
-
-utils = (function()
-
-  -- Save copied tables in `copies`, indexed by original table.
-  function deepcopy(orig, copies)
-    copies = copies or {}
-    local orig_type = type(orig)
-    local copy
-    if orig_type == 'table' then
-        if copies[orig] then
-            copy = copies[orig]
-        else
-            copy = {}
-            copies[orig] = copy
-            for orig_key, orig_value in next, orig, nil do
-                copy[deepcopy(orig_key, copies)] = deepcopy(orig_value, copies)
-            end
-            setmetatable(copy, deepcopy(getmetatable(orig), copies))
-        end
-    else -- number, string, boolean, etc
-        copy = orig
-    end
-    return copy
-  end
-
-	local t = {
-        map = function(tbl, f)
-            local t = {}
-            for k,v in pairs(tbl) do
-                t[k] = f(v,k)
-            end
-            return t
-	    end,
-      filter = function(tbl,f)
-          local t = {}
-          for k,v in pairs(tbl) do
-              if f(v,k) then t[k] = v; end
-          end
-          return t
-      end,
-      -- Save copied tables in `copies`, indexed by original table.
-      copy = deepcopy
-  }
-    return {table = t}
-end)()
-
+os.loadAPI("/utils.lua")
+utils = utils.utils
 
 --[[API FACTORY]]
 
@@ -135,24 +88,13 @@ Style = class({
 local Elements = {}
 local focusedElement = false
 
-	Element = (function()
-	
-  local EventLoop = function(event)
-    focusedElement = false
-    for i=#Elements,1,-1 do
-      local v = Elements[i]
-      if v[event[1]] then
-        v[event[1]](v,event)
-      end
-    end
-  end
-  
-  
+Element = (function()
+
   local Element = class({
-  focused = false,
-  children = {},
-  content = false,
-  constructor = function(self,tag,props,content)
+    focused = false,
+    children = {},
+    content = false,
+    constructor = function(self,tag,props,content)
       for k,v in pairs(props) do
         self[k] = v
       end
@@ -160,73 +102,73 @@ local focusedElement = false
       self.tag = tag
       self.content = content or self.content
       Elements[#Elements+1] = self
-  end,
-  getElementsByClassName = function(className)
-    return table.filter(Elements,function(v)
-      return v.class == className
-    end)
-  end,
-  appendChild = function(self,_element)
+    end,
+    getElementsByClassName = function(className)
+      return table.filter(Elements,function(v)
+        return v.class == className
+      end)
+    end,
+    appendChild = function(self,_element)
       _element.parent = self
       if _element.style.backgroundColor == "transparent" then
         _element.style.backgroundColor = self.style.backgroundColor     
       end
       table.insert(self.children, _element)
-  end,
-  prependChild = function(self,_element)
-    table.insert(self.children,1,_element)
-  end,
-  setFocus  = function(self,event)
-    self.focused = true
-  end,
-  mouse_click = function(self,event)
-    self.focused = false
-    local left,top,right,bottom = self:getBounds()
-    local x = event[3]
-    local y = event[4]
-    if x > left - 1 and x < right + 1 and y > top - 1 and y < bottom + 1 then   
+    end,
+    prependChild = function(self,_element)
+      table.insert(self.children,1,_element)
+    end,
+    setFocus  = function(self,event)
       self.focused = true
-      if self.onClick and focusedElement == false then
-            self:setFocus(event)
-            self:onClick(event)
+    end,
+    mouse_click = function(self,event)
+      self.focused = false
+      local left,top,right,bottom = self:getBounds()
+      local x = event[3]
+      local y = event[4]
+      if x > left - 1 and x < right + 1 and y > top - 1 and y < bottom + 1 then   
+        self.focused = true
+        if self.onClick and focusedElement == false then
+          self:setFocus(event)
+          self:onClick(event)
         end
         focusedElement = true
       end
-  end,
-  -- Return the Bounding Box that will be rendered for the element
-  getBounds = function(self)
-
-    local offsetLeft = 1
-    local offsetTop = 1
-    if self.parent then
-      offsetLeft = self.parent.offsetLeft + self.parent.style.marginLeft + self.parent.style.left + self.parent.style.paddingLeft - 1
-      offsetTop = self.parent.offsetTop + self.parent.style.marginTop + self.parent.style.top + self.parent.style.paddingTop
-    end
-    local style = self.style
-    -- update the sides to include position, and margins
-    offsetLeft = offsetLeft + style.left + style.marginLeft 
-    offsetTop = offsetTop + style.top + style.marginTop
-    offsetRight = offsetLeft + style.width + style.paddingLeft + style.paddingRight
-    offsetBottom = offsetTop +  style.height + style.paddingTop + style.paddingBottom - 1
-
-    self.offsetLeft = offsetLeft
-    self.offsetTop = offsetTop
-    -- for k,v in pairs(utils.table.filter(self.children,function(t)return t.style.display ~= "none";end)) do
-    --      pleft,pTop,pRight,pBottom = v:getBounds()
-    --      offsetRight = math.max(offsetRight, pRight + style.paddingRight)
-    --      offsetBottom = math.max(offsetBottom, pBottom + style.paddingBottom)
-    -- end 
-    return offsetLeft ,offsetTop, offsetRight, offsetBottom
-  end,
-  render = function(self)
-
-    -- Set Local Variable to override if neccessary
-    local style = self.style
-    local color = style.color
-    local backgroundColor = style.backgroundColor
-    local left,top,right,bottom = self:getBounds()
-
-    if style.display ~= "none" then
+    end,
+    -- Return the Bounding Box that will be rendered for the element
+    getBounds = function(self)
+      offsetLeft = 1
+      offsetTop = 1
+     
+      if self.parent then
+        offsetLeft = self.parent.offsetLeft + self.parent.style.marginLeft + self.parent.style.left + self.parent.style.paddingLeft - 1
+        offsetTop = self.parent.offsetTop + self.parent.style.marginTop + self.parent.style.top + self.parent.style.paddingTop
+      end
+      local style = self.style
+      -- update the sides to include position, and margins
+      offsetLeft = offsetLeft + style.left + style.marginLeft 
+      offsetTop = offsetTop + style.top + style.marginTop
+      offsetRight = offsetLeft + style.width + style.paddingLeft + style.paddingRight
+      offsetBottom = offsetTop +  style.height + style.paddingTop + style.paddingBottom - 1
+      
+      self.offsetLeft = offsetLeft
+      self.offsetTop = offsetTop
+      -- for k,v in pairs(utils.table.filter(self.children,function(t)return t.style.display ~= "none";end)) do
+      --      pleft,pTop,pRight,pBottom = v:getBounds()
+      --      offsetRight = math.max(offsetRight, pRight + style.paddingRight)
+      --      offsetBottom = math.max(offsetBottom, pBottom + style.paddingBottom)
+      -- end 
+      return offsetLeft ,offsetTop, offsetRight, offsetBottom
+    end,
+    render = function(self)
+      
+      -- Set Local Variable to override if neccessary
+      local style = self.style
+      local color = style.color
+      local backgroundColor = style.backgroundColor
+      local left,top,right,bottom = self:getBounds()
+      
+      if style.display ~= "none" then
         if style.backgroundColor ~= "transparent" then
           ccraft.paintutils.drawFilledBox(left,top,right,bottom,self.style.backgroundColor)
         else
@@ -236,18 +178,20 @@ local focusedElement = false
         end
         if self.content then
           ccraft.term.setBackgroundColor(self.style.backgroundColor)
-          ccraft.term.setTextColor(color)      
+          ccraft.term.setTextColor(color)     
           ccraft.term.setCursorPos(left + style.paddingLeft+1,top + style.paddingTop)
           ccraft.term.write(self.content)
           ccraft.term.setCursorPos(10,10)
         end;
         for k,v in ipairs(self.children) do
           v:render()
+          ccraft.term.setBackgroundColor(ccraft.colors.black)
+          ccraft.term.setTextColor(ccraft.colors.white)      
         end
+      end
     end
-  end
   })
-
+  
   Element.root = false
   Element.div = class({
     constructor = function(self,props,content)
@@ -270,7 +214,7 @@ local focusedElement = false
       end
     end
   },Element)
-
+  
   Element.input = class({
     constructor = function(self,props,content)
       self.super.constructor(self,"input",props,content)
@@ -285,79 +229,143 @@ local focusedElement = false
       self.content = "";
     end,
     onChange = function(self)
-
+      
     end,
     char = function(self,event)
       if self.focused then
         self:change(event[2])
       end
     end
-  	},Element)
+  },Element)
   Element.createElement = function(tag,props,content)
     return Element[tag].new(props,content)
   end
-
-  Element.startEventLoop = function()
-    while true do
-      Element.root:render()
-      event = {os.pullEvent()}
-      EventLoop(event)
-    end
-  end
+  
+  
   Element.attachRoot = function(child)
     Element.root = child
     Element.startEventLoop()
   end
+  Element.triggerEvent = function(event)
+    focusedElement = false
+    for _,v in pairs(Elements) do
+      if v[event[1]] then
+        v[event[1]](v,event)
+      end
+    end
+  end
+
   return Element
 end)()
 
 React = (function()
   
   local react = {}
+  local rootComponent = function()end
+  local rootElement = {}
   local hookStorage = {}
+  local oldStorage = {}
+  local refStorage = {}
   local hookIndex = 1
-  local rerender = function()
-  local render = function()
-    hookIndex = 1
-  end
-  react.useState = function(startState)
-      local state = hooksStorage[hookIndex] or startState
-      local frozenIndex = hookIndex
-      local setState = function(newVal)
-          if type(newVal) == "function" then
-          newVal = newVal(hooksStorage[frozenIndex])
-          hookStorage[frozenIndex] = newVal
-          return newVal
-      end
-      hookIndex++
+  local virtualDom = {}
+
+  react.renderElement = function(c)
+    local child = nil
+    el = Element[c.tag].new(c.props,"")
+    el.children = {}
+    if type(c.props.children) ~= "table" then
+      el.content = c.props.children
+    else
+      for _,v in ipairs(c.props.children) do
+        if v.props then
+          child = react.renderElement(v)
+        else
+          child = v
+        end
+        
+        el:appendChild(child)
+      end      
     end
-    return react
-  end
-  react.render = function(c)
-    hookIndex = 1
-    local el = c
-    if type(c) == "function" then
-      local comp = c()
-      local content = nil
-      if type(comp.children) ~= "table" then
-        content = props.children
-      end
-      local el = Element.createElement(comp.tag,comp.props,content)
-    end
-    if type(props.children) == "table" then
-      for _,v in pairs(props.children) do
-        el:appendChild(v)
-      end
-    do
+    utils.table.save(el,el.id)
     return el
   end
-  react.renderDom = function(component,rootElement)
-    react.root = el
-    react.render(el)
-    rootElement:appendChild(el)
-    Element.startEventLoop()
+
+  react.render = function(c)
+    hookIndex = 1
+    
+    comp = react.renderElement(c())
+    return comp
   end
+  local rerender = function()
+      ccraft.term.setBackgroundColor(ccraft.colors.black)
+      local el = react.render(rootComponent)
+      if utils.table.is(oldStorage,hookStorage) == false then
+        oldStorage = utils.table.copy(hookStorage)
+        rootElement.children = {}
+        rootElement:appendChild(el)
+        Elements = {rootElement}
+        rootElement:render()
+      end
+  end
+  react.useState = function(startState)
+    local state = hookStorage[hookIndex] or startState
+   
+    local frozenIndex = hookIndex
+    local setState = function(newVal)
+      if type(newVal) == "function" then
+        newVal = newVal(hookStorage[frozenIndex])
+      end
+      hookStorage[frozenIndex] = newVal
+      return newVal
+    end
+    hookIndex = hookIndex + 1
+    if hookStorage[hookIndex-1] then return hookStorage[hookIndex-1],setState end
+    return state,setState
+  end
+  react.useRef = function(val)
+    local state = react.useState({current = val})
+    return state
+  end
+  react.useEffect = function(cb,depArray)
+    local oldDeps = hookStorage[hookIndex]
+    local hasChanged = false
+    
+    if oldDeps then
+      hasChanged = utils.table.is(depArray,oldDeps) == false
+    end
+    if hasChanged then cb() end
+    hookStorage[hookIndex] = depArray
+  end
+  react.startWorkLoop = function()
+      timer = os.startTimer(1)
+      while true do
+        event = {os.pullEvent()}
+        Element.triggerEvent(event)
+        if event[1] == "timer" and event[2] == timer then
+          rerender()
+          timer = os.startTimer(1)
+        end
+      end
+  end
+  react.renderDom = function(component,re,norender) 
+    rootElement = re
+    rootComponent = component
+    local element = react.render(rootComponent)
+    rootElement:appendChild(element)
+    rootElement:render()
+    if norender then return end
+    react.startWorkLoop()
+    Element.attachRoot(rootElement)
+  end
+
+  react.Element = class({
+    constructor = function(self,tag,props,key,ref)
+        self.tag = tag
+        self.props = props
+        self.key = key
+        self.ref = ref
+    end
+  })
+
   return react
-end)
-
-
+end)()
--- startup.lua
+++ startup.lua
@@ -1,25 +1,84 @@
 os.loadAPI("/Library.lua")
+Element = Library.Element
+cc = Library.ccraft
+utils = Library.utils
+Style = Library.Style
 React = Library.React
 RenderDom = React.renderDom
-Element = Library.Element
-CC = Element.ccraft
-e = React.createElement
+e = React.Element.new
 
-WIDTH, HEIGHT = CC.term.getSize()
+WIDTH, HEIGHT = cc.term.getSize()
 root = Element.createElement("div",{
+    style = Style.new({
     width = WIDTH,
     height = HEIGHT,
-    background = CC.colors.blue
+    backgroundColor = cc.colors.blue
+    })
 })
 
+--[[ Desktop Component ]]
+
+BeastOs = function(props)
+    return Element.button.new(
+    {
+        id="beastos",
+        style = Style.new({
+            paddingRight = 1,
+            left = WIDTH - 8,
+            height = 1,
+            width = 8,
+            color = cc.colors.lightBlue
+        }), 
+        onClick = function(self,event)
+            props.setText("another")
+        end,
+        content = "beast"
+    })
+end
+
+StartBtn = function(props)
+    return Element.button.new({
+        id = "startBtn",
+        style = Style.new({
+            width = 7,
+            height = 2,
+            left = 1,
+            backgroundColor = cc.colors.lightGray
+        }),
+        onClick = function(self,event)
+            props.setText("update")
+        end,
+        content = props.text
+    })
+end
+TaskBar = function(props)
+
+    local taskBarStyle = Style.new({
+        height = 1,
+        width = WIDTH,
+        backgroundColor = cc.colors.red,
+        textColor = cc.colors.black
+    })
+    return e("div",{
+        id = "taskbar",
+        style = taskBarStyle,
+        children = {StartBtn({text = props.text,setText = props.setText}),BeastOs({text = props.text,setText = props.setText})}
+    })
+end
 App = function()
-    return {
-        tag = "div",
-        props = {
-            children = "hello"
-        },
-        key = 1
-    }
+    local text,setText = React.useState("[start]")
+    local secondText,setSecondText = React.useState("[anotherone]")
+    local renderCount = React.useRef(1)
+    React.useEffect(function()
+       renderCount.current = renderCount.current + 1
+    end,{text})
+
+    return TaskBar({
+        secondText = secondText,
+        setSecondText = setSecondText,
+        text = text .. renderCount.current,
+        setText = setText
+    })
 end
 
 RenderDom(App,root)
